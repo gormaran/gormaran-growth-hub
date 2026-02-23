2b80f4bda6e0c3c6c81f5fdaf572c98bf31df115
Gabriela Ormazabal <gormaran@outlook.es>
2026-02-23 20:09:28 +0100

    Add multilanguage selector (EN/ES/FR/DE/IT) to Navbar

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -3,6 +3,9 @@ import { initReactI18next } from "react-i18next";
 
 import en from "./en.json";
 import es from "./es.json";
+import fr from "./fr.json";
+import de from "./de.json";
+import it from "./it.json";
 
 i18n
   .use(initReactI18next)
@@ -10,8 +13,11 @@ i18n
     resources: {
       en: { translation: en },
       es: { translation: es },
+      fr: { translation: fr },
+      de: { translation: de },
+      it: { translation: it },
     },
-    lng: "en",
+    lng: localStorage.getItem("language") || "en",
     fallbackLng: "en",
     interpolation: {
       escapeValue: false,
